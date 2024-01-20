0a004504ecdce1e583212ca1da466f744f6ffc12
Jericho Sharman <jsharman@hotmail.com.au>
2024-01-20 22:47:48 +1100

    sql

--- HOME_INVENTORY_DATABASE/test.sql
+++ HOME_INVENTORY_DATABASE/test.sql
@@ -1,20 +1,27 @@
-DROP TABLE IF EXISTS MANUFATURERS CASCADE;
-DROP TABLE IF EXiSTS PURCHASE_HISTORY CASCADE;
-DROP TABLE IF EXiSTS STORES CASCADE;
-DROP TABLE IF EXiSTS LOCATION_MASTER CASCADE;
-DROP TABLE IF EXiSTS SOH CASCADE;
-DROP TABLE IF EXISTS ITEM_TYPES CASCADE;CREATE TABLE ITEM_TYPES (
-    type_id INT PRIMARY KEY
-);
-DROP TABLE IF EXISTS PRIME_LOCATION CASCADE;
+/*  The HOME INVENTORY DATABASE SCHEMA
+    Designed & Created  by Jericho Sharman 01/2024
+*/
+DROP TABLE IF EXISTS PURCHASE_HISTORY,STORES,LOCATION_MASTER,SOH,ITEM_TYPES,PRIME_LOCATION,ITEM_MASTER,MANUFACTURERS,ITEM_IMAGES_URLS CASCADE;
 
+CREATE TABLE ITEM_TYPES (type_id INT PRIMARY KEY);
+CREATE TABLE manufacturers (
+	manufacturer_ID INT AUTO_INCREMENT PRIMARY KEY,
+    manufacturer_name VARCHAR(50),
+    manufacturer_description VARCHAR(100),
+    manufacturer_number INT  /*  random field, use for anything */
+);
 CREATE TABLE PRIME_LOCATION (
     location_id INT PRIMARY KEY,
     prime_location_name varchar(32),
     primt_location_description VARCHAR(120)
 );
-
-DROP TABLE IF EXiSTS ITEM_MASTER CASCADE;CREATE TABLE ITEM_MASTER (
+CREATE TABLE LOCATION_MASTER(
+	location_id INT AUTO_INCREMENT PRIMARY KEY,
+    location_name VARCHAR(25),
+    location_photo varchar(150),
+    location_desc varchar(150)
+);
+CREATE TABLE ITEM_MASTER (
     item_number INT PRIMARY KEY,
     alt_item_number INT,
     descr varchar(64),
@@ -24,10 +31,18 @@ DROP TABLE IF EXiSTS ITEM_MASTER CASCADE;CREATE TABLE ITEM_MASTER (
     item_width INT,
     image_link_ID INT,
     item_type int REFERENCES ITEM_TYPES (type_id),
-    manufacturer_ID INT
+    manufacturer_ID INT REFERENCES manufacturers(man_id),
+    files varchar(20) /*here you will keep file names such as PDF files etc.    */
 );
+CREATE TABLE SOH (
+	soh_id INT AUTO_INCREMENT PRIMARY KEY,
+    soh_item INT REFERENCES ITEM_MASTER(item_number),
+    soh_location INT REFERENCES LOCATION_MASTER(location_id)
+)
+/*The image table.   */
 
-DROP TABLE IF EXISTS ITEM_IMAGES_URLS CASCADE;CREATE TABLE ITEM_IMAGES_URLS (
-    the_item INT REFERENCES ITEM_MASTER(item_number),
-    the_image_url varchar(112)
+CREATE TABLE ITEM_IMAGES_URLS (
+    the_item INT REFERENCES ITEM_MASTER(item_number), /*The item unique identifier (PRIMARY KEY)    */
+    the_image_url varchar(112),  /* A web link to the image. Consider using online storage perhaps Cloudinary or similiar*/
+    the_image_description varchar(150)
 )
