0ffcc0805071a4136cbbcf543ee93dc63b3f70b9
Jericho <jsharman@hotmail.com.au>
2024-01-30 13:59:08 +1100

    shouldnt update

--- HOME_INVENTORY_DATABASE/SCHEMA/user_management.sql
+++ HOME_INVENTORY_DATABASE/SCHEMA/user_management.sql
@@ -1,13 +1,16 @@
-CREATE TABLE user_types { user_type_id INT AUTO INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS users, database_roles,user_types CASCADE;
+
+CREATE TABLE user_types (
+user_type_id INT AUTO_INCREMENT PRIMARY KEY,
 user_type_description TEXT,
 user_type_field1 INT,
-user_type_field2 INT };
+user_type_field2 INT );
 
 CREATE TABLE database_roles (
-    role_id int AUTO INCREMENT PRIMARY KEY,
+    role_id int AUTO_INCREMENT PRIMARY KEY,
     role_desc TEXT,
     role_delete INT,
-    role_add INT,
+    role_add INT
 );
 
 CREATE TABLE users (
@@ -19,5 +22,6 @@ CREATE TABLE users (
     user_hashed_pwd varchar(100),
     user_mailing_list tinyINT,
     user_last_login DATETIME,
-    user_type INT REFERENCES user_types (user_typeID) user_database_role int REFERENCES database_roles (role_id)
+    user_type INT REFERENCES user_types (user_typeID),
+    user_database_role int REFERENCES database_roles (role_id)
 );
