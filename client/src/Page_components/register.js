9de348c37f34a50a54ee94b8ae614f0f7decec89
Jericho <jsharman@hotmail.com.au>
2024-02-09 18:33:26 +1100

    adding a palette.css and context variables

--- client/src/Page_components/register.js
+++ client/src/Page_components/register.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './styles/login.css';
+import './styles/form.css';
 import axios from 'axios'; // Import Axios
 function RegisterForm({ setLogin }) {
   const [firstname, setFirstname] = useState('');
