e2ec90bec3c8548553e0e513c84fff2b9ad95990
Jericho <jsharman@hotmail.com.au>
2024-02-21 18:36:27 +1100

    starting soh_card

--- client/src/Components/showinventory/showInventory.js
+++ client/src/Components/showinventory/showInventory.js
@@ -3,9 +3,9 @@
   Created by Jericho Sharman
   Data is passed as a prop 
 */
-import React from 'react';
-import './styles/inventoryitem.css'
-import RowComponent from './rowComponent';
+import React, { useState } from "react";
+import "./styles/inventoryitem.css";
+import RowComponent from "./rowComponent";
 // function formatDateTime(dateTimeString) {
 //   const date = new Date(dateTimeString);
 //   // Format the date as needed, for example: DD/MM/YYYY HH:mm:ss
@@ -13,18 +13,22 @@ import RowComponent from './rowComponent';
 //   return formattedDateTime;
 // }
 function ShowInventory(props) {
+
+  const [viewpage,setviewpage] = useState(1)
   const dataArray = Object.values(props.theData);
   // const headings = ['Item Name', 'Location', 'Qty', 'Description', 'Picture']  // ID in the CSS file must match these headings. Spaces are replaced with a hythen '-' for use in the CSS file
   return (
     <div>
       <div className="inventory-container">
-        {dataArray.map((item, index) => (
-          <div className="data-row-container">
-            <RowComponent {...item}/>
-          </div>
-        ))}
+        {viewpage === 1
+          ? dataArray.map((item, index) => (
+              <div key={index} className="data-row-container">
+                <RowComponent {...item} />
+              </div>
+            ))
+          : "nothing"}
       </div>
     </div>
-  )
+  );
 }
 export default ShowInventory;
